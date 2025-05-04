b6f05c2d07b58d95c0e36a9e933b8094a6640a53
Marco-developer-cyber <muxammad.004.com@gmail.com>
2025-05-05 03:10:11 +0500

    Удалил JSON

--- my-app/src/Components/CyberpunkLoader.tsx
+++ my-app/src/Components/CyberpunkLoader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import './CyberpunkLoader.css';
+import './Styles/CyberpunkLoader.css';
 
 const CyberpunkLoader: React.FC = () => {
   return (
